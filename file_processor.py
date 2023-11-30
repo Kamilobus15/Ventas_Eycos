56391ef8f0b8b1389ba8d5917a2517a583257c3f
Ivan Camilo Bustos Mateus <79616279+Kamilobus15@users.noreply.github.com>
2023-11-30 17:30:26 -0500

    ya terminamos esta parte wiiiii

--- file_processor.py
+++ file_processor.py
@@ -11,14 +11,20 @@ def custom_encoder(obj):
     if isinstance(obj, str):
         return obj.encode('utf-8').decode('unicode-escape')
     return obj
-
+# Aplicar la función de codificación al DataFrame
 df_encoded = df.applymap(custom_encoder)
 
-# 'records' orientation is often used for a list of dictionaries
+#acá hacemos la suma 
+df['Cant'] = -df['Cant']
+df['Valor_Unitario'] = -df['Valor_Unitario']
+df['Subtotal'] = -df['Subtotal']
+
+
+# ''records' orientation se utiliza comúnmente para una lista de diccionariosords' orientation is often used for a list of dictionaries
 json_data = df.to_json(orient='records', force_ascii=False,  indent=4,  date_format='iso')
 
 # Save JSON to a file
-with open('anual2023.json', 'w', encoding='utf-8') as json_file:
+with open('anual2023_pruba.json', 'w', encoding='utf-8') as json_file:
     json_file.write(json_data)
 
 # Print JSON to the console
